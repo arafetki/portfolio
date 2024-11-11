dba3264513f3902b656c33a5708a72ba4245930e
Arafet Ben Kilani <arafet.benkilani@supcom.tn>
2024-11-11 23:26:29 +0100

    updated env schema

--- src/env.ts
+++ src/env.ts
@@ -9,7 +9,7 @@ export const env = createEnv({
       .default("development"),
     RESEND_API_KEY: z.string().min(1),
     RESEND_AUDIENCE_ID: z.string().min(1),
-    DATABASE_URL: z.string().url(),
+    DATABASE_URL: z.string().min(1).url(),
     JWT_SECRET_KEY: z.string().min(1),
   },
   client: {},
