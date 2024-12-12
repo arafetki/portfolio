ae6bb3292abc314de177aeadfe4711a94ee8910e
Arafet Ben Kilani <arafet.benkilani@supcom.tn>
2024-12-12 20:46:51 +0100

    disabled copytoclipboard temporary

--- src/components/code-block.tsx
+++ src/components/code-block.tsx
@@ -1,48 +1,39 @@
 "use client";
 
-import React from "react";
-import ClipBoardButton from "@/components/clipboard-button";
+// import { ReactNode, isValidElement } from "react";
+// import ClipBoardButton from "@/components/clipboard-button";
 import { cn } from "@/lib/utils";
 
 type CodeBlockProps = React.DetailsHTMLAttributes<HTMLPreElement>;
 
-function extractTextFromNode(node: React.ReactNode): string {
-  // Base case: if the node is a string, return it.
-  if (typeof node === "string") return node;
+// function extractTextFromNode(node: ReactNode): string {
+//   // Base case: if the node is a string, return it.
+//   if (typeof node === "string") return node;
 
-  // Handle numbers or other primitive types that could be direct children.
-  if (typeof node === "number") return node.toString();
+//   // Handle numbers or other primitive types that could be direct children.
+//   if (typeof node === "number") return node.toString();
 
-  // Check if the node is an object and has the required properties.
-  if (!React.isValidElement(node) || !node.props || !node.props.children) {
-    return "";
-  }
+//   // Check if the node is an object and has the required properties.
+//   if (!isValidElement(node) || !node.props || !node.props.children) {
+//     return "";
+//   }
 
-  // Skip removed lines of code based on className.
-  if (node.props.className?.includes("remove")) {
-    return "__line_removed_in_code__";
-  }
+//   // Recursively handle child nodes (whether an array or single element).
+//   const { children } = node.props;
+//   if (Array.isArray(children)) {
+//     return children.map(extractTextFromNode).join("");
+//   }
 
-  // Recursively handle child nodes (whether an array or single element).
-  const { children } = node.props;
-  if (Array.isArray(children)) {
-    return children.map(extractTextFromNode).join("");
-  }
-
-  // Process a single child node if it’s not an array.
-  return extractTextFromNode(children);
-}
+//   // Process a single child node if it’s not an array.
+//   return extractTextFromNode(children);
+// }
 
 export default function CodeBlock({
   children,
   className,
   ...rest
 }: CodeBlockProps) {
-  const code = extractTextFromNode(children).replace(
-    /(\n)?__line_removed_in_code__(\n)?/g,
-    ""
-  );
-
+  // const code = extractTextFromNode(children);
   return (
     <div className="relative my-6 rounded-lg bg-stone-950 dark:bg-stone-800/35">
       <pre
@@ -51,7 +42,7 @@ export default function CodeBlock({
       >
         {children}
       </pre>
-      <ClipBoardButton text={code} className="absolute right-1 top-1" />
+      {/* <ClipBoardButton text={code} className="absolute right-1 top-1" /> */}
     </div>
   );
 }
